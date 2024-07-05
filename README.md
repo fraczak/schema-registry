594f8ea2171058fcaaeb75196e46847c15566a99
wojtek <fraczak@gmail.com>
2024-07-04 20:32:19 -0400

    WIP

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Ultimate Schema Registry using `k` codes.
 
 ```bash 
     curl -X POST -H Content-Type: application/json -d @Examples/nat.json http://localhost:3000/spec
+    curl -X POST -H "Content-Type: text/plain; charset=UTF-8" --data-binary @yourfile.txt http://example.com/endpoint
+
 ```
--- index.mjs
+++ index.mjs
@@ -1,30 +1,37 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import json_file_object from 'json-file-object';
 import k from 'k';
 import { encodeCodeToString, fingerprint } from 'k/fingerprint.mjs'
 
 const schemas = json_file_object({file:'schemas.json'});
+const rels_by_dom = json_file_object({file:'rels-by-dom.json'});
+const rels_by_img = json_file_object({file:'rels-by-img.json'});
 
 const app = express();
-app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
+const body_as_string = express.text();
+
 // Endpoint to store a schema
-app.post('/spec', (req, res) => {
-  const script = req.body.script;
+app.post('/script', body_as_string, (req, res) => {
+  const script = req.body;
   const annotated = k.annotate(script);
-  const result = {new: [], existing: []};
+
+  console.log(JSON.stringify(annotated,null,2));
+  const aliases = Object.keys(annotated.representatives).reduce((acc, x) => {
+    const rep = annotated.representatives[x];
+    acc[rep] ? acc[rep].push(x) : acc[rep] = [x];
+    return acc;
+  }, {});
+
+  const result = {new: {}, existing: {}};
   for (const code in annotated.codes) {
-    console.log(`// CODE ALIAS : ${code}`);
     const s = encodeCodeToString(code,annotated.codes);
-    console.log(`// DEFINITION : ${s}`);
     const name = fingerprint(s);
-    console.log(`// FINGERPRINT: ${name}`);
     if (schemas[name]) {
       if (s !== schemas[name]) {
-        console.log(`// CONFLICT: ${s} != ${schemas[name]}`);
+        console.error(`CONFLICT: ${s} != ${schemas[name]}`);
         return res.status(409).json({
           error: 'Two defs for the same code!', 
           code: name, 
@@ -32,9 +39,9 @@ app.post('/spec', (req, res) => {
           new: s
         });
       };
-      result.existing.push({alias:code, name:name});
+      result.existing[name] = aliases[code];
     } else {
-      result.new.push({alias:code, name:name});
+      result.new[name] = aliases[code];
       schemas[name] = s;
     }
   }
@@ -50,7 +57,11 @@ app.get('/schema/:name', (req, res) => {
         return res.status(404).json({ error: 'Schema not found' });
     }
 
-    res.status(200).json({ name: schemaName, spec: schema});
+    res.status(200).json({ id: schemaName, spec: schema});
+});
+
+app.get('/all', (req, res) => {
+    res.status(200).json(schemas);
 });
 
 app.listen(PORT, () => {
